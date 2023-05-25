11ff0ef1361abcaa022ae0a4ae1faef8396a21dd
badmood111 <petrzela.adam@seznam.cz>
2023-05-25 14:49:51 +0200

    Annuity calculation tests + method

--- src/main/dev/adampeter/calculator/Calculator.java
+++ src/main/dev/adampeter/calculator/Calculator.java
@@ -1,8 +1,45 @@
 package dev.adampeter.calculator;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class Calculator {
 
     public int add(int num1, int num2) {
         return num1 + num2;
+    }
+
+    public int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public int divide(int num1, int num2) {
+        if (num2 != 0) {
+            return num1 / num2;
+        }
+        return -1;
+    }
+
+    public String calcAnnuity(String R, int t, String r, int n) {
+        NumberFormat moneyFormatter = NumberFormat.getCurrencyInstance(Locale.US);
+
+        BigDecimal bigDecimalR = new BigDecimal(R);
+        BigDecimal bigDecimalT = new BigDecimal(t);
+        BigDecimal rate = new BigDecimal(r);
+        BigDecimal bigDecimalN = new BigDecimal(n);
+
+        BigDecimal rateDivN = rate.divide(bigDecimalN); //We don't need a MathContext in this example
+        BigDecimal onePlusRateDivN = rateDivN.add(BigDecimal.valueOf(1));
+        BigDecimal up = onePlusRateDivN.pow(bigDecimalN.multiply(bigDecimalT).intValue()).subtract(BigDecimal.valueOf(1));
+        BigDecimal result = bigDecimalR.multiply((up.divide(rateDivN)));
+
+        return moneyFormatter.format(result);
     }
 }
--- src/test/dev/adampeter/calculator/CalculatorTest.java
+++ src/test/dev/adampeter/calculator/CalculatorTest.java
@@ -1,6 +1,7 @@
 package dev.adampeter.calculator;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,23 +15,99 @@ public class CalculatorTest {
         calc = new Calculator();
     }
 
-    //we can name the test anything (don't have to include the word test) - we have to Annotate the test method
-    public
-    @Test void canAddZeroPlusZero() {
+    //ADD
+    @Test
+    public void canAddZeroPlusZero() {
         int sum = calc.add(0, 0);
         //Assertion of equality of expected and calculated values (+ error message)
         assertEquals(0, sum, "Was expecting sum of 0");
     }
 
-    public
-    @Test void canAddOnePlusOne() {
+    @Test
+    public void canAddOnePlusOne() {
         int sum = calc.add(1, 1);
         assertEquals(2, sum);
     }
 
-    public
-    @Test void canAddNegativeNumbers() {
+    @Test()
+    @Disabled // or @Ignore (earlier version) - means we don't want to delete the test but also don't want to fix it now
+    public void canAddMaxIntPlusOne() {
+        int sum = calc.add(Integer.MAX_VALUE, 1);
+        assertEquals(Integer.MAX_VALUE + 1L, sum);
+    }
+
+    @Test
+    public void canAddNegativeNumbers() {
         int sum = calc.add(0, -1);
         assertEquals(-1, sum);
+    }
+
+    //SUBTRACT
+    @Test
+    public void subtractZero() {
+        int diff = calc.subtract(0, 0);
+        assertEquals(0, diff);
+    }
+
+    @Test
+    public void subtractOne() {
+        int diff = calc.subtract(0, 1);
+        assertEquals(-1, diff);
+    }
+
+    @Test
+    public void subtractNegative() {
+        int diff = calc.subtract(0, -1);
+        assertEquals(1, diff);
+    }
+
+    //MULTIPLY
+    @Test
+    public void multiplyByZero() {
+        int result = calc.multiply(0, 0);
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void multiplyByTwo() {
+        int result = calc.multiply(5, 2);
+        assertEquals(10, result);
+    }
+
+    @Test
+    public void multiplyByNegative() {
+        int result = calc.multiply(5, -2);
+        assertEquals(-10, result);
+    }
+
+    //DIVIDE
+    @Test
+    public void divideByZero() {
+        int result = calc.divide(1, 0);
+        assertEquals(-1, result);
+    }
+
+    @Test
+    public void divideByPositive() {
+        int result = calc.divide(2, 2);
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void divideByNegative() {
+        int diff = calc.multiply(8, -2);
+        assertEquals(-16, diff);
+    }
+
+    @Test
+    public void annuityExample() {
+        String answer = calc.calcAnnuity("22000", 7, ".06", 1);
+        assertEquals("$184,664.43", answer);
+    }
+
+    @Test
+    public void annuityPractice2() {
+        String answer = calc.calcAnnuity("1200", 10, ".08", 4);
+        assertEquals("$72,482.38", answer);
     }
 }
